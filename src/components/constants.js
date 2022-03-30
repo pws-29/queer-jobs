96292b55350353d7b9e478a66f8e985f726045d4
Pietro Weg <pietrows29@gmail.com>
2022-03-30 11:45:20 -0300

    feat: initial arch created

---
+++ src/components/constants.js
@@ -0,0 +1,22 @@
+export const COLORS = {
+  black: '#000000',
+  white: '#ffffff',
+  primary: '#fcbf49',
+  secondary: '#2b566d',
+  grey: {
+    primary: '#f3f2f5',
+    secondary: '#a4a4a4'
+  },
+};
+
+export const WEIGHTS = {
+  light: 300,
+  regular: 400,
+  bold: 700,
+};
+
+export const RADIUS = {
+  sm: '4px',
+  md: '8px',
+  lg: '16px',
+};
