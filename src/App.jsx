237591e02541446c3da29d709a4cc39ef9517511
Wilciara <wilciara@gmail.com>
2022-03-31 21:25:30 -0300

    http://...

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Link from "./components/atoms/Link"
 
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+
+      <Link type="headerLink" target="https://www.google.com.br">Para candidatos</Link>
+      <br/>
+      <Link type="bodyLink" target="https://www.google.com.br">Cadastrar minha empresa</Link>
+      
+
     </>
   );
 };
---
+++ src/components/atoms/Link.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import {COLORS, WEIGHTS} from '../constants'
+
+
+function Link({children, type, target}) {
+    return (
+        <LinkStyled className={type} href={target}>{children}</LinkStyled>
+    )
+}
+
+const LinkStyled = styled.a`
+    cursor: pointer;
+
+    &.headerLink{
+        color: ${COLORS.black};
+        font-weight: ${WEIGHTS.regular};
+        font-size: 1.125rem;
+        text-decoration: none;
+    }
+
+    &.bodyLink{
+        color: ${COLORS.secondary};
+        font-weight: ${WEIGHTS.bold};
+        font-size: 1rem;
+        text-decoration: underline;
+    }
+
+`
+
+export  default Link;
