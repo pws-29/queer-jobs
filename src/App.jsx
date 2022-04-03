8abb697fe7f732e753b772e985df1183073f00b9
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2022-04-03 15:25:14 -0300

    feat: link-atom added with changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ function App() {
 
   return (
     <>
-
-      <Link type="headerLink" target="https://www.google.com.br">Para candidatos</Link>
-      <br/>
-      <Link type="bodyLink" target="https://www.google.com.br">Cadastrar minha empresa</Link>
-      
-
+      <h1>Hello World!</h1>
     </>
   );
 };
