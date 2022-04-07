7df80c704b9cc8a27652a160520cbd7ba8da93ce
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2022-04-07 20:59:34 -0300

    feat: numbers-card added

---
+++ src/components/molecules/NumbersCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import TextPlaceholder from '../atoms/TextPlaceholder';
+import { COLORS } from '../constants';
+import Spacer from '../atoms/Spacer';
+
+/**
+ * ### Parâmetros
+ * 
+ * @param {string} title - Título do card - suggested_value: '+500';
+ * @param {string} description - Descrição do card;
+ * 
+ */
+function NumbersCard({ title, description }) {
+  return (
+    <Wrapper>
+      <TextPlaceholder>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </TextPlaceholder>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  h1 {
+      background-color: ${COLORS.primary.normal};
+      width: fit-content;
+      padding: 4px 8px;
+    }
+  `
+
+export default NumbersCard
