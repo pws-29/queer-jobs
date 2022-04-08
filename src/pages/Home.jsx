72e5c3cdf83a12ae24015997f71ee53d5dfaab36
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2022-04-07 21:51:42 -0300

    feat: info-card organism added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import MainContent from "../components/organisms/home/MainContent";
+import InfoContent from "../components/organisms/home/InfoContent";
 import PlansContent from "../components/organisms/home/PlansContent";
 import NumbersContent from "../components/organisms/home/NumbersContent";
 import Spacer from "../components/atoms/Spacer";
@@ -7,10 +8,12 @@ function Home() {
   return (
     <>
       <MainContent />
-      <Spacer size={100} />
+      <Spacer size={120} />
       <PlansContent />
-      <Spacer size={100} />
+      <Spacer size={120} />
       <NumbersContent />
+      <Spacer size={120} />
+      <InfoContent />
     </>
   );
 };
